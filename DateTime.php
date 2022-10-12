0a22442d1b4b4c95aa58242adfe373c1b7d7a16a
Fedor Zhuravlev <zhuravlev_fa@drom.ru>
2022-10-12 11:30:18 +1000

    Добавил примеры работы с date, time, strtotime, getdate

--- DateTime.php
+++ DateTime.php
@@ -1,24 +1,52 @@
 <?php
 
-$date = date_create();
+//$date = date_create();
+//
+//// не выдаст ошибку, доп дни запишет в октябрь
+//$date->setDate(2022, 9, 40);
+//var_dump($date);
+//// даж в ошибку не запишет :/
+//var_dump($date::getLastErrors());
+//
+//// поменяет
+//$date->setDate(2022, 9, 30);
+//var_dump($date);
+//
+//// относительный формат даты
+//$date2 = new DateTime('next monday');
+//var_dump($date2);
+//
+//$date3 =date_create('first sat of July 2008');
+//var_dump($date3);
+//
+//// составной формат
+//$date4 = DateTime::createFromFormat('d.m.Y h:i:s.u', '03.10.2022 12:30:28.12313');
+//var_dump($date4);
 
-// не выдаст ошибку, доп дни запишет в октябрь
-$date->setDate(2022, 9, 40);
-var_dump($date);
-// даж в ошибку не запишет :/
-var_dump($date::getLastErrors());
+// день недели
+echo date('l') . PHP_EOL;
 
-// поменяет
-$date->setDate(2022, 9, 30);
-var_dump($date);
+// день недели, число месяц год, врема
+echo date('l jS \of F Y h:i:s A') . PHP_EOL;
 
-// относительный формат даты
-$date2 = new DateTime('next monday');
-var_dump($date2);
+// дата + время
+echo date(DATE_ATOM) . PHP_EOL;
 
-$date3 =date_create("first sat of July 2008");
-var_dump($date3);
+// ассоциативный массив с датой
+print_r(getdate());
 
-// составной формат
-$date4 = DateTime::createFromFormat('d.m.Y h:i:s.u', '03.10.2022 12:30:28.12313');
-var_dump($date4);
+// эквивалентно time()
+echo 'strtotime:' . strtotime('now'). PHP_EOL;
+echo 'time:' . time(). PHP_EOL;
+
+echo strtotime('10 September 2000'). PHP_EOL;
+
+echo strtotime('+1 day'). PHP_EOL;
+
+echo strtotime('+1 week'). PHP_EOL;
+
+echo strtotime('+1 week 2 days 4 hours 2 seconds'). PHP_EOL;
+
+echo strtotime('next Thursday'). PHP_EOL;
+
+echo strtotime('last Monday'). PHP_EOL;
